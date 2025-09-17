ae08004018536583851e1ece8d46d791a28f8904
joe95-ux <ogorktabi@gmail.com>
2025-09-17 23:06:10 +0200

    use command component

--- components/places.tsx
+++ components/places.tsx
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
 
-
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+
 import {
   Command,
-  CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 
 type PlacesProps = {
   setOffice: (position: google.maps.LatLngLiteral) => void;
@@ -36,8 +29,6 @@ export default function Places({ setOffice }: PlacesProps) {
     clearSuggestions,
   } = usePlacesAutocomplete();
 
-  const [open, setOpen] = useState<boolean>(false);
-
   const handleSelect = async (val: string) => {
     setValue(val, false);
     clearSuggestions();
@@ -48,55 +39,41 @@ export default function Places({ setOffice }: PlacesProps) {
   };
 
   return (
-    <div className="flex flex-col gap-4">
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[100%] justify-between bg-background hover:bg-accent hover:text-accent-foreground"
-          >
-            {value
-              ? data.find((location) => location.description.includes(value))
-                  ?.description
-              : "Search office address..."}
-            <ChevronsUpDown className="opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[100%] p-0  border border-border bg-popover text-popover-foreground">
-          <Command>
-            <CommandInput
-              placeholder="Search office address..."
-              className="h-9 border-b border-border"
-              value={value}
-              onValueChange={(value:string) => setValue(value)}
-              disabled={!ready}
-            />
-            <CommandList>
-              <CommandEmpty className="py-6 text-center text-sm text-muted-foreground">No office address found.</CommandEmpty>
-              <CommandGroup>
-                {status === "OK" &&
-                  data.map(({ description, place_id }) => (
-                    <CommandItem
-                      key={place_id}
-                      value={description}
-                      onSelect={handleSelect}
-                    >
-                      {description}
-                      <Check
-                        className={cn(
-                          "ml-auto",
-                          value === description ? "opacity-100" : "opacity-0"
-                        )}
-                      />
-                    </CommandItem>
-                  ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
+    <div className="flex flex-col gap-4 w-full">
+      <Command className="rounded-lg border border-slate-800 bg-slate-900/90 shadow-lg w-full text-slate-100">
+        <CommandInput
+          placeholder="Search office address..."
+          className="h-9 border-b border-slate-700 bg-slate-900/70 text-slate-100 placeholder:text-slate-400 focus:ring-0"
+          value={value}
+          onValueChange={(value: string) => setValue(value)}
+          disabled={!ready}
+        />
+        {value && (
+          <CommandList className="bg-slate-900/90">
+            <CommandGroup>
+              {status === "OK" &&
+                data.map(({ description, place_id }) => (
+                  <CommandItem
+                    key={place_id}
+                    value={description}
+                    onSelect={handleSelect}
+                    className="hover:bg-slate-800 cursor-pointer text-slate-400"
+                  >
+                    {description}
+                    <Check
+                      className={cn(
+                        "ml-auto text-slate-400",
+                        value === description
+                          ? "text-slate-200 opacity-100"
+                          : "opacity-0"
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+            </CommandGroup>
+          </CommandList>
+        )}
+      </Command>
     </div>
   );
 }
